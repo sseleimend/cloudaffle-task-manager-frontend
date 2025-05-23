3e621c60fd79e8525030c1bf11fb97d27cc19c45
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 12:20:28 -0300

    Update signup.jsx by adding the page layout

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,3 +1,29 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Signup() {
-  return <>Signup Page</>;
+  return (
+    <section className="flex flex-row w-full max-w-7xl min-h-screen justify-center items-center">
+      <div className="w-4/12">
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
+  );
 }
