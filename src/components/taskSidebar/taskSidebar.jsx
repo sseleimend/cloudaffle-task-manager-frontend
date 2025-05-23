8d00a93ce83ca8f0bf9546d06b4a8bf1558fa912
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 17:05:36 -0300

    Update taskSidebar.jsx by increasing the minimum width

--- src/components/taskSidebar/taskSidebar.jsx
+++ src/components/taskSidebar/taskSidebar.jsx
@@ -7,7 +7,7 @@ import { Logout } from "../logout/logout.jsx";
 export function TaskSidebar() {
   return (
     <section
-      className={`fixed right-4 top-4 max-h-screen ${styles.sidebarHeight} min-w-96`}
+      className={`fixed right-4 top-4 max-h-screen ${styles.sidebarHeight} min-w-lg`}
     >
       <Card className="flex flex-col h-full w-full p-6 justify-between">
         <UserProfile />
