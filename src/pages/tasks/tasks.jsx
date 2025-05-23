c869ce12880a50f7dcef7d1d660c858e19b951be
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 13:37:51 -0300

    Fix the alignment of the task page

--- src/pages/tasks/tasks.jsx
+++ src/pages/tasks/tasks.jsx
@@ -5,16 +5,18 @@ export default function Tasks() {
   return (
     <section className="flex flex-row w-full p-4 gap-8">
       <section className="flex  basis-2/3 justify-center">
-        <div className="flex flex-col w-4/5 p-4">
-          <h1 className="font-bold text-2xl mb-8">
+        <div className="flex flex-col w-10/12 p-4 items-center">
+          <h1 className="font-bold text-2xl mb-8 w-full">
             Tasks as on: Wednesday, 1 Jan 2025
           </h1>
-          <div className="flex justify-around mb-16">
-            <TasksCounter count={4} type="todo" />
-            <TasksCounter count={10} type="inProgress" />
-            <TasksCounter count={12} type="completed" />
+          <div className="w-11/12 flex flex-col">
+            <div className="flex justify-between mb-16">
+              <TasksCounter count={4} type="todo" />
+              <TasksCounter count={10} type="inProgress" />
+              <TasksCounter count={12} type="completed" />
+            </div>
+            <FilterBar />
           </div>
-          <FilterBar />
         </div>
       </section>
       <section className="flex basis-1/3"></section>
