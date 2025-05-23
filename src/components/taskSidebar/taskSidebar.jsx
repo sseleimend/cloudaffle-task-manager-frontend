51c6f2d168b769d8ffc8ae6dd328f771adf895af
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 16:05:16 -0300

    Update the taskSidebar by adding user profile, task form creation and logout components

--- src/components/taskSidebar/taskSidebar.jsx
+++ src/components/taskSidebar/taskSidebar.jsx
@@ -1,5 +1,8 @@
 import { Card } from "@/components//ui/card.jsx";
 import styles from "./taskSidebar.module.css";
+import { UserProfile } from "../userProfile/userProfile.jsx";
+import { CreateTaskForm } from "../createTaskForm/createTaskForm.jsx";
+import { Logout } from "../logout/logout.jsx";
 
 export function TaskSidebar() {
   return (
@@ -7,9 +10,9 @@ export function TaskSidebar() {
       className={`fixed right-4 top-4 max-h-screen ${styles.sidebarHeight} min-w-96`}
     >
       <Card className="flex flex-col h-full w-full p-6 justify-between">
-        <p>User Profile</p>
-        <p>Form</p>
-        <p>Logout</p>
+        <UserProfile />
+        <CreateTaskForm />
+        <Logout />
       </Card>
     </section>
   );
