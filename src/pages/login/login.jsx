7f38f2d42d178c5e8e3493ee523cee10358e3fda
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 18:59:05 -0300

    Add login validation

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -9,8 +9,26 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schema/login.schema.js";
 
 export default function Login() {
+  const form = useForm({
+    resolver: zodResolver(LoginSchema),
+  });
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
   return (
     <section className="flex flex-row w-full max-w-7xl min-h-screen justify-center items-center">
       <div className="w-4/12">
@@ -19,19 +37,49 @@ export default function Login() {
             <CardTitle>Login</CardTitle>
             <CardDescription>Login and create tasks</CardDescription>
           </CardHeader>
-          <CardContent>
-            <Input className="mb-4" type="email" placeholder="Email" />
-            <Input className="mb-4" type="password" placeholder="Password" />
-          </CardContent>
-          <CardFooter className="flex flex-row justify-between">
-            <p className="basis-1/2">
-              Don’t have an account?{" "}
-              <Link to="/signup" className="text-blue-500">
-                Signup Here
-              </Link>
-            </p>
-            <Button>Login</Button>
-          </CardFooter>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <CardContent>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input placeholder="Email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="Password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </CardContent>
+              <CardFooter className="flex flex-row justify-between">
+                <p className="basis-1/2">
+                  Don’t have an account?{" "}
+                  <Link to="/signup" className="text-blue-500">
+                    Signup Here
+                  </Link>
+                </p>
+                <Button type="submit">Login</Button>
+              </CardFooter>
+            </form>
+          </Form>
         </Card>
       </div>
     </section>
