e2dfc9e18c8ad3e0d493a0f4dc2866dc207064d7
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 18:01:20 -0300

    Add logout component

--- src/components/logout/logout.jsx
+++ src/components/logout/logout.jsx
@@ -1,3 +1,12 @@
+import { LogOut } from "lucide-react";
+import { Button } from "../ui/button.jsx";
+
 export function Logout() {
-  return <div>Logout</div>;
+  return (
+    <div className="flex justify-end">
+      <Button variant="outline" size="icon">
+        <LogOut className="size-4" />
+      </Button>
+    </div>
+  );
 }
