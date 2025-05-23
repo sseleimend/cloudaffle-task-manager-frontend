7125f7eaff547abd23e5f0a5ef52bd95dee35682
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 13:05:48 -0300

    Update tasks.jsx by adding the page layout

--- src/pages/tasks/tasks.jsx
+++ src/pages/tasks/tasks.jsx
@@ -1,3 +1,19 @@
 export default function Tasks() {
-  return <>Tasks Page</>;
+  return (
+    <section className="flex flex-row w-full p-4 gap-8">
+      <section className="flex  basis-2/3 justify-center">
+        <div className="flex flex-col w-4/5 p-4">
+          <h1 className="font-bold text-2xl mb-8">
+            Tasks as on: Wednesday, 1 Jan 2025
+          </h1>
+          <div className="flex justify-around">
+            <p>Counter 1</p>
+            <p>Counter 2</p>
+            <p>Counter 3</p>
+          </div>
+        </div>
+      </section>
+      <section className="flex basis-1/3"></section>
+    </section>
+  );
 }
