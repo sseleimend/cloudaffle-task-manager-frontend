715f50be7fd6d04eb82a9342e70f637e291083a7
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 16:29:53 -0300

    Add user profile component

--- src/components/userProfile/userProfile.jsx
+++ src/components/userProfile/userProfile.jsx
@@ -1,3 +1,14 @@
-export function UserProfile() {
-  return <div>User Profile</div>;
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export function UserProfile({ firstName = "John" }) {
+  return (
+    <div className="flex flex-col w-full py-8 items-center">
+      <Avatar className="mb-4">
+        <AvatarFallback className="text-2xl font-semibold">
+          {firstName.slice(0, 1)}
+        </AvatarFallback>
+      </Avatar>
+      <h4>Hello, {firstName}</h4>
+    </div>
+  );
 }
