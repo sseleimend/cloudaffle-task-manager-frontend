985376208ea1ad10a382db2dea44f27c04a45130
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 17:05:09 -0300

    Update userProfile.jsx replacing vertical padding with top padding

--- src/components/userProfile/userProfile.jsx
+++ src/components/userProfile/userProfile.jsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export function UserProfile({ firstName = "John" }) {
   return (
-    <div className="flex flex-col w-full py-8 items-center">
+    <div className="flex flex-col w-full pt-8 items-center">
       <Avatar className="mb-4">
         <AvatarFallback className="text-2xl font-semibold">
           {firstName.slice(0, 1)}
