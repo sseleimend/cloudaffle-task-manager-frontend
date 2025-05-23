ed64d1d2d0838423a4871c4a0c215d61ea07a90b
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 12:57:51 -0300

    Update login.jsx by adding the page layout

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,17 +1,39 @@
+import { Button } from "@/components/ui/button.jsx";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { Link } from "react-router";
 
 export default function Login() {
   return (
-    <>
-      <h1>This is a login page</h1>
-      <ul>
-        <li>
-          <Link to="tasks">Tasks</Link>
-        </li>
-        <li>
-          <Link to="signup">Signup</Link>
-        </li>
-      </ul>
-    </>
+    <section className="flex flex-row w-full max-w-7xl min-h-screen justify-center items-center">
+      <div className="w-4/12">
+        <Card>
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Login and create tasks</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Input className="mb-4" type="email" placeholder="Email" />
+            <Input className="mb-4" type="password" placeholder="Password" />
+          </CardContent>
+          <CardFooter className="flex flex-row justify-between">
+            <p className="basis-1/2">
+              Don’t have an account?{" "}
+              <Link to="/signup" className="text-blue-500">
+                Signup Here
+              </Link>
+            </p>
+            <Button>Login</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
   );
 }
