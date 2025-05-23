de24815d0521f19069edcbc7ea66ea5fea7cbee5
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 17:58:04 -0300

    Add create task form component

--- src/components/createTaskForm/createTaskForm.jsx
+++ src/components/createTaskForm/createTaskForm.jsx
@@ -1,3 +1,95 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input.jsx";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea.jsx";
+
 export function CreateTaskForm() {
-  return <div>Task Form</div>;
+  const [date, setDate] = useState();
+
+  return (
+    <div>
+      <h2 className="text-xl mb-4">Create a new task</h2>
+      <form>
+        <div className="py-2">
+          <Input type="text" placeholder="Task Title" />
+        </div>
+        <div className="flex flex-row justify-between py-2">
+          <div className="mr-2 w-full">
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Status" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="todo">Todo</SelectItem>
+                  <SelectItem value="inProgress">In Progress</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="ml-2 w-full">
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Priority" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="normal">Normal</SelectItem>
+                  <SelectItem value="low">Low</SelectItem>
+                  <SelectItem value="high">High</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <div className="py-2">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-full justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date ? format(date, "PPP") : <span>Due Date</span>}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+              <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="py-2">
+          <Textarea placeholder="Description of the task" />
+        </div>
+        <div className="py-2 flex justify-end">
+          <Button>Create Task</Button>
+        </div>
+      </form>
+    </div>
+  );
 }
