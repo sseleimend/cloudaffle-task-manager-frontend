844f76bf97bce2925b7ab0e028a3ca7ffc059229
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 18:48:54 -0300

    Add signup form validation

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -9,8 +9,28 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { SignupSchema } from "@/schema/signup.schema.js";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 export default function Signup() {
+  const form = useForm({
+    resolver: zodResolver(SignupSchema),
+  });
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
   return (
     <section className="flex flex-row w-full max-w-7xl min-h-screen justify-center items-center">
       <div className="w-4/12">
@@ -21,21 +41,73 @@ export default function Signup() {
               Create a new account to start creating tasks
             </CardDescription>
           </CardHeader>
-          <CardContent>
-            <Input className="mb-4" type="text" placeholder="First Name" />
-            <Input className="mb-4" type="text" placeholder="Last Name" />
-            <Input className="mb-4" type="email" placeholder="Email" />
-            <Input className="mb-4" type="password" placeholder="Password" />
-          </CardContent>
-          <CardFooter className="flex flex-row justify-between">
-            <p className="basis-1/2">
-              Already have an account?{" "}
-              <Link to="/" className="text-blue-500">
-                Login Here
-              </Link>
-            </p>
-            <Button>Signup</Button>
-          </CardFooter>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <CardContent>
+                <FormField
+                  control={form.control}
+                  name="firstName"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input placeholder="First Name" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="lastName"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input placeholder="Last Name" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input placeholder="Email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="mb-4">
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="Password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </CardContent>
+              <CardFooter className="flex flex-row justify-between">
+                <p className="basis-1/2">
+                  Already have an account?{" "}
+                  <Link to="/" className="text-blue-500">
+                    Login Here
+                  </Link>
+                </p>
+                <Button>Signup</Button>
+              </CardFooter>
+            </form>
+          </Form>
         </Card>
       </div>
     </section>
