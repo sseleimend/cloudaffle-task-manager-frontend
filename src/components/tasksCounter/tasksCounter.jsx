e556abe16acf8ff36af3ab74280cbddff5f5cf90
Victor Seleimend <sseleimend@gmail.com>
2025-05-23 13:13:04 -0300

    Add tasks counter

---
+++ src/components/tasksCounter/tasksCounter.jsx
@@ -0,0 +1,12 @@
+export function TasksCounter() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="p-6 border-solid border-red-500 border-4 rounded-full mb-4">
+        <div className="min-w-10 min-h-10 text-3xl text-center flex justify-center leading-10">
+          12
+        </div>
+      </div>
+      <div>Todo</div>
+    </div>
+  );
+}
--- src/pages/tasks/tasks.jsx
+++ src/pages/tasks/tasks.jsx
@@ -1,3 +1,5 @@
+import { TasksCounter } from "@/components/tasksCounter/tasksCounter.jsx";
+
 export default function Tasks() {
   return (
     <section className="flex flex-row w-full p-4 gap-8">
@@ -7,9 +9,9 @@ export default function Tasks() {
             Tasks as on: Wednesday, 1 Jan 2025
           </h1>
           <div className="flex justify-around">
-            <p>Counter 1</p>
-            <p>Counter 2</p>
-            <p>Counter 3</p>
+            <TasksCounter />
+            <TasksCounter />
+            <TasksCounter />
           </div>
         </div>
       </section>
