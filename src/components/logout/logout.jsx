1ae50d1adc2e1f66173be30f5da1248d90765678
Victor Seleimend <sseleimend@gmail.com>
2025-05-24 13:16:37 -0300

    Update the logout component by implementing its behavior

--- src/components/logout/logout.jsx
+++ src/components/logout/logout.jsx
@@ -1,10 +1,19 @@
 import { LogOut } from "lucide-react";
 import { Button } from "../ui/button.jsx";
+import { useNavigate } from "react-router";
+import Cookies from "js-cookie";
 
 export function Logout() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    Cookies.remove("token");
+    navigate("/");
+  }
+
   return (
     <div className="flex justify-end">
-      <Button variant="outline" size="icon">
+      <Button onClick={handleClick} variant="outline" size="icon">
         <LogOut className="size-4" />
       </Button>
     </div>
