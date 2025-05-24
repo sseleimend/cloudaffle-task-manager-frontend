db4074ab7046cd51cc37c15ea4e6040ed4728e1f
Victor Seleimend <sseleimend@gmail.com>
2025-05-24 12:20:20 -0300

    Update the 404 page by removing the h1 color

--- src/pages/404/404.jsx
+++ src/pages/404/404.jsx
@@ -1,7 +1,7 @@
 export default function Error404() {
   return (
     <>
-      <h1 className="text-4xl text-black font-bold">404 - Page Not Found</h1>
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
       <p>The page you are trying to access does not exist</p>
     </>
   );
